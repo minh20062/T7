15dab679f9d0bb57e73b2f87a191f79ef8700b24
hahahaahh <minh18032006@gmail.com>
2025-06-28 12:08:19 +0700

    feat: thêm form đăng nhập và logo góc phải

--- frontend/src/pages/login/LoginForm.jsx
+++ frontend/src/pages/login/LoginForm.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import axios from "../../services/api";
 import { useNavigate } from "react-router-dom";
-import "./LoginForm.css"; 
+import "./LoginForm.css";
+ 
 
 
 export default function LoginForm(){
@@ -40,19 +41,23 @@ export default function LoginForm(){
 
    return (
      <div className="login-container">
+      <div className="logo-container">
+      <img src="/assets/logo.jpg" alt="Logo" className="logo-img" />
+      <span className="brand-name">MINHCOMPANY</span>
+    </div>
       <form className="login-form" onSubmit={handleLogin}>
         <h2>LOGIN</h2>
         {errorMsg && <p className="error">{errorMsg}</p>}
         <input
           type="email"
-          placeholder="Nhập Email"
+          placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
         <input
           type="password"
-          placeholder="Nhập mật khẩu"
+          placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
